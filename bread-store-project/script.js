77e8853523c481c87fc4b552dd0e953c8ad1cc9e
fadhil0523 <fadhil0523@students.abudzar.sch.id>
2025-11-06 13:32:56 +0700

    bakery

---
+++ bread-store-project/script.js
@@ -0,0 +1,15 @@
+// let tagline = document.getElementById('tagline')
+// window.addEventListener('scroll', ()=> {
+//     let value = window.scrollY;
+//     console.log('value');
+    
+//     tagline.style.marginTop = value *2.5+'px';
+// })
+
+let tagline = document.getElementById('tagline')
+window.addEventListener('scroll', ()=> {
+    let headline = window.scrollY;
+    console.log('headline');
+
+    tagline.style.marginTtop = value *2.5+'px';
+})
